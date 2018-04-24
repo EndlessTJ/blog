c7d24e5641d89c9c19effdc4a89f97399eccb6c8
jinta <1294546949@qq.com>
2018-04-24 12:00:24 +0800

    create:create front-end folder

--- admin/src/PostEdit/PostEdit.jsx
+++ admin/src/PostEdit/PostEdit.jsx
@@ -10,20 +10,28 @@ class Postedit extends Component {
           <form className="postedit-form">
             <div className="postedit-form-body">
               <label className="form-label">
-                <span className="form-input-label">文章标题</span>
-                <input type="text" placeholder="文章标题" />
+                <span className="form-input-label">文章标题:</span>
+                <input
+                  type="text"
+                  className="form-control form-input"
+                  placeholder="文章标题"
+                />
               </label>
               <label className="form-label">
-                <span className="form-input-label">作者</span>
-                <input type="text" placeholder="请输入作者" />
+                <span className="form-input-label">作者:</span>
+                <input
+                  type="text"
+                  className="form-control form-input"
+                  placeholder="请输入作者"
+                />
               </label>
               <label className="form-label">
-                <span className="form-textarea-label">文章内容</span>
-                <textarea />
+                <span className="form-textarea-label">文章内容:</span>
+                <textarea className="form-control form-textarea" />
               </label>
-              <label className="from-label">
-                <span className="from-input-label">选择标签</span>
-                <select name="">
+              <label className="form-label">
+                <span className="form-input-label">选择标签:</span>
+                <select name="" className="form-control form-select">
                   <option value="">标签1</option>
                   <option value="">标签2</option>
                   <option value="">标签3</option>
@@ -32,12 +40,12 @@ class Postedit extends Component {
                 </select>
               </label>
               <label className="form-label">
-                <span className="from-input-label">是否顶置</span>
-                <input type="checkbox" />
+                <span className="form-input-label">是否顶置:</span>
+                <input type="checkbox" className="form-control form-radio" />
               </label>
               <label className="from-label">
-                <span className="from-input-label">是否推荐</span>
-                <input type="checkbox" />
+                <span className="form-input-label">是否推荐:</span>
+                <input type="checkbox" className="form-control form-radio" />
               </label>
             </div>
           </form>
