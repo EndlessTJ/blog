fe39f1eb6682e68be215e09fe321d7f4a5649e94
EndlessTJ <1294546949@qq.com>
2018-12-20 16:57:11 +0800

    some fixed

--- public/lib/dbconnect.js
+++ public/lib/dbconnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/TJblog');
+mongoose.connect('mongodb://localhost:27017/TJblog',{ useNewUrlParser: true });
 const db = mongoose.connection;
 
 
